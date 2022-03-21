4067da1a27bcb24e0712eb5beef62c492c2b5da1
Leolopes013 <leo_piula@hotmail.com>
2022-03-20 22:16:09 -0300

    Commit Tabuada

---
+++ TabuadaDesejada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabuadaDesejada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Informe um número..: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i+" x "+numero+" = "+(numero*i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
